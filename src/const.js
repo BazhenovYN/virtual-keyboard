1743ec28988964c6ab159364690f997fb85b44bd
BazhenovYN <BazhenovYN@gmail.com>
2023-04-27 00:42:03 +0300

    feat: keyboard layout done

---
+++ src/const.js
@@ -0,0 +1,523 @@
+const firstLine = [
+  {
+    keyCode: 192,
+    code: 'backquote',
+    label: {
+      en: '`',
+      ru: 'Ё',
+    },
+  },
+  {
+    keyCode: 49,
+    code: 'digit1',
+    label: {
+      en: '1',
+      ru: '1',
+    },
+  },
+  {
+    keyCode: 50,
+    code: 'digit2',
+    label: {
+      en: '2',
+      ru: '2',
+    },
+  },
+  {
+    keyCode: 51,
+    code: 'digit3',
+    label: {
+      en: '3',
+      ru: '3',
+    },
+  },
+  {
+    keyCode: 52,
+    code: 'digit4',
+    label: {
+      en: '4',
+      ru: '4',
+    },
+  },
+  {
+    keyCode: 53,
+    code: 'digit5',
+    label: {
+      en: '5',
+      ru: '5',
+    },
+  },
+  {
+    keyCode: 54,
+    code: 'digit6',
+    label: {
+      en: '6',
+      ru: '6',
+    },
+  },
+  {
+    keyCode: 55,
+    code: 'digit7',
+    label: {
+      en: '7',
+      ru: '7',
+    },
+  },
+  {
+    keyCode: 56,
+    code: 'digit8',
+    label: {
+      en: '8',
+      ru: '8',
+    },
+  },
+  {
+    keyCode: 57,
+    code: 'digit9',
+    label: {
+      en: '9',
+      ru: '9',
+    },
+  },
+  {
+    keyCode: 48,
+    code: 'digit0',
+    label: {
+      en: '0',
+      ru: '0',
+    },
+  },
+  {
+    keyCode: 189,
+    code: 'minus',
+    label: {
+      en: '-',
+      ru: '-',
+    },
+  },
+  {
+    keyCode: 187,
+    code: 'equal',
+    label: {
+      en: '=',
+      ru: '=',
+    },
+  },
+  {
+    keyCode: 8,
+    code: 'backspace',
+    label: {
+      en: 'BACKSPACE',
+      ru: 'BACKSPACE',
+    },
+  },
+];
+const secondLine = [
+  {
+    keyCode: 9,
+    code: 'tab',
+    label: {
+      en: 'TAB',
+      ru: 'TAB',
+    },
+  },
+  {
+    keyCode: 81,
+    code: 'KeyQ',
+    label: {
+      en: 'Q',
+      ru: 'Й',
+    },
+  },
+  {
+    keyCode: 87,
+    code: 'keyW',
+    label: {
+      en: 'W',
+      ru: 'Ц',
+    },
+  },
+  {
+    keyCode: 69,
+    code: 'keyE',
+    label: {
+      en: 'E',
+      ru: 'У',
+    },
+  },
+  {
+    keyCode: 82,
+    code: 'keyR',
+    label: {
+      en: 'R',
+      ru: 'К',
+    },
+  },
+  {
+    keyCode: 84,
+    code: 'keyT',
+    label: {
+      en: 'T',
+      ru: 'Е',
+    },
+  },
+  {
+    keyCode: 89,
+    code: 'keyY',
+    label: {
+      en: 'Y',
+      ru: 'Н',
+    },
+  },
+  {
+    keyCode: 85,
+    code: 'keyU',
+    label: {
+      en: 'U',
+      ru: 'Г',
+    },
+  },
+  {
+    keyCode: 73,
+    code: 'keyI',
+    label: {
+      en: 'I',
+      ru: 'Ш',
+    },
+  },
+  {
+    keyCode: 79,
+    code: 'keyO',
+    label: {
+      en: 'O',
+      ru: 'Щ',
+    },
+  },
+  {
+    keyCode: 80,
+    code: 'keyP',
+    label: {
+      en: 'P',
+      ru: 'З',
+    },
+  },
+  {
+    keyCode: 219,
+    code: 'bracketLeft',
+    label: {
+      en: '[',
+      ru: 'Х',
+    },
+  },
+  {
+    keyCode: 221,
+    code: 'bracketRight',
+    label: {
+      en: ']',
+      ru: 'Ъ',
+    },
+  },
+  {
+    keyCode: 220,
+    code: 'backslash',
+    label: {
+      en: '\\',
+      ru: '\\',
+    },
+  },
+];
+const thirdLine = [
+  {
+    keyCode: 20,
+    code: 'capsLock',
+    label: {
+      en: 'CAPS LOCK',
+      ru: 'CAPS LOCK',
+    },
+  },
+  {
+    keyCode: 65,
+    code: 'keyA',
+    label: {
+      en: 'A',
+      ru: 'Ф',
+    },
+  },
+  {
+    keyCode: 83,
+    code: 'keyS',
+    label: {
+      en: 'S',
+      ru: 'Ы',
+    },
+  },
+  {
+    keyCode: 68,
+    code: 'keyD',
+    label: {
+      en: 'D',
+      ru: 'В',
+    },
+  },
+  {
+    keyCode: 70,
+    code: 'keyF',
+    label: {
+      en: 'F',
+      ru: 'А',
+    },
+  },
+  {
+    keyCode: 71,
+    code: 'keyG',
+    label: {
+      en: 'G',
+      ru: 'П',
+    },
+  },
+  {
+    keyCode: 72,
+    code: 'keyH',
+    label: {
+      en: 'H',
+      ru: 'Р',
+    },
+  },
+  {
+    keyCode: 74,
+    code: 'keyJ',
+    label: {
+      en: 'J',
+      ru: 'О',
+    },
+  },
+  {
+    keyCode: 75,
+    code: 'keyK',
+    label: {
+      en: 'K',
+      ru: 'Л',
+    },
+  },
+  {
+    keyCode: 76,
+    code: 'keyL',
+    label: {
+      en: 'L',
+      ru: 'Д',
+    },
+  },
+  {
+    keyCode: 186,
+    code: 'semicolon',
+    label: {
+      en: ';',
+      ru: 'Ж',
+    },
+  },
+  {
+    keyCode: 222,
+    code: 'quote',
+    label: {
+      en: '\'',
+      ru: 'Э',
+    },
+  },
+  {
+    keyCode: 13,
+    code: 'enter',
+    label: {
+      en: 'ENTER',
+      ru: 'ENTER',
+    },
+  },
+];
+const forthLine = [
+  {
+    keyCode: 16,
+    code: 'shiftLeft',
+    label: {
+      en: 'SHIFT',
+      ru: 'SHIFT',
+    },
+  },
+  {
+    keyCode: 90,
+    code: 'keyZ',
+    label: {
+      en: 'Z',
+      ru: 'Я',
+    },
+  },
+  {
+    keyCode: 88,
+    code: 'keyX',
+    label: {
+      en: 'X',
+      ru: 'Ч',
+    },
+  },
+  {
+    keyCode: 67,
+    code: 'keyC',
+    label: {
+      en: 'C',
+      ru: 'С',
+    },
+  },
+  {
+    keyCode: 86,
+    code: 'keyV',
+    label: {
+      en: 'V',
+      ru: 'М',
+    },
+  },
+  {
+    keyCode: 66,
+    code: 'keyB',
+    label: {
+      en: 'B',
+      ru: 'И',
+    },
+  },
+  {
+    keyCode: 78,
+    code: 'keyN',
+    label: {
+      en: 'N',
+      ru: 'Т',
+    },
+  },
+  {
+    keyCode: 77,
+    code: 'keyM',
+    label: {
+      en: 'M',
+      ru: 'Ь',
+    },
+  },
+  {
+    keyCode: 188,
+    code: 'comma',
+    label: {
+      en: ',',
+      ru: 'Б',
+    },
+  },
+  {
+    keyCode: 190,
+    code: 'period',
+    label: {
+      en: '.',
+      ru: 'Ю',
+    },
+  },
+  {
+    keyCode: 191,
+    code: 'slash',
+    label: {
+      en: '/',
+      ru: '.',
+    },
+  },
+  {
+    keyCode: 16,
+    code: 'shiftRight',
+    label: {
+      en: 'SHIFT',
+      ru: 'SHIFT',
+    },
+  },
+];
+const fifthLine = [
+  {
+    keyCode: 17,
+    code: 'controlLeft',
+    label: {
+      en: 'CTRL',
+      ru: 'CTRL',
+    },
+  },
+  {
+    keyCode: 91,
+    code: 'metaLeft',
+    label: {
+      en: 'WIN',
+      ru: 'WIN',
+    },
+  },
+  {
+    keyCode: 18,
+    code: 'altLeft',
+    label: {
+      en: 'ALT',
+      ru: 'ALT',
+    },
+  },
+  {
+    keyCode: 32,
+    code: 'space',
+    label: {
+      en: '',
+      ru: '',
+    },
+  },
+  {
+    keyCode: 18,
+    code: 'altRight',
+    label: {
+      en: 'ALT',
+      ru: 'ALT',
+    },
+  },
+  {
+    keyCode: 17,
+    code: 'controlRight',
+    label: {
+      en: 'CTRL',
+      ru: 'CTRL',
+    },
+  },
+  {
+    keyCode: 37,
+    code: 'arrowLeft',
+    label: {
+      en: '←',
+      ru: '←',
+    },
+  },
+  {
+    keyCode: 38,
+    code: 'arrowUp',
+    label: {
+      en: '↑',
+      ru: '↑',
+    },
+  },
+  {
+    keyCode: 40,
+    code: 'arrowDown',
+    label: {
+      en: '↓',
+      ru: '↓',
+    },
+  },
+  {
+    keyCode: 39,
+    code: 'arrowRight',
+    label: {
+      en: '→',
+      ru: '→',
+    },
+  },
+];
+
+export const APP_TITLE = 'RSS Virtual Keyboard';
+export const keyLayout = [
+  firstLine,
+  secondLine,
+  thirdLine,
+  forthLine,
+  fifthLine,
+];
--- src/index.js
+++ src/index.js
@@ -1,2 +1,106 @@
 import './normalize.css';
 import './style.scss';
+import { APP_TITLE, keyLayout } from './const';
+
+class Keyboard {
+  constructor() {
+    this.elements = {
+      main: null,
+      keysContainer: null,
+      keys: [],
+    };
+    this.properties = {
+      value: '',
+      capsLock: false,
+      language: 'en',
+    };
+  }
+
+  init() {
+    // Create main elements
+    this.elements.main = document.createElement('div');
+    this.elements.keysContainer = document.createElement('div');
+
+    // Setup main elements
+    this.elements.main.classList.add('keyboard');
+    this.elements.keysContainer.classList.add('keyboard__keys');
+    this.elements.keysContainer.appendChild(this.createKeys());
+
+    this.elements.keys = this.elements.keysContainer.querySelectorAll('.keyboard__key');
+
+    // Add to DOM
+    const title = document.createElement('h2');
+    title.innerText = APP_TITLE;
+    title.classList.add('title');
+
+    const textArea = document.createElement('textarea');
+    textArea.classList.add('text');
+
+    this.elements.main.appendChild(title);
+    this.elements.main.appendChild(textArea);
+    this.elements.main.appendChild(this.elements.keysContainer);
+    document.body.appendChild(this.elements.main);
+  }
+
+  createKeys() {
+    const fragment = document.createDocumentFragment();
+
+    const light = document.createElement('span');
+    light.classList.add('light');
+
+    keyLayout.forEach((keyRow) => {
+      const keyLine = document.createElement('div');
+      keyLine.classList.add('keyboard__line');
+
+      keyRow.forEach((key) => {
+        const keyElement = document.createElement('button');
+        keyElement.classList.add('keyboard__key');
+        keyElement.id = key.code.toLowerCase();
+
+        switch (key.code) {
+          case 'backspace':
+            keyElement.textContent = key.label[this.properties.language];
+            break;
+          case 'capsLock':
+            keyElement.textContent = key.label[this.properties.language];
+            keyElement.appendChild(light);
+            break;
+          default:
+            keyElement.textContent = key.label[this.properties.language];
+            keyElement.addEventListener('click', () => {
+              this.properties.value += this.properties.capsLock
+                ? key.label[this.properties.language].toUpperCase()
+                : key.label[this.properties.language];
+            });
+
+            break;
+        }
+
+        keyLine.appendChild(keyElement);
+      });
+
+      fragment.appendChild(keyLine);
+    });
+    return fragment;
+  }
+}
+
+const keyboard = new Keyboard();
+
+window.addEventListener('DOMContentLoaded', () => {
+  keyboard.init();
+});
+
+document.addEventListener('keydown', (event) => {
+  const activeButton = document.querySelector(`#${event.code.toLowerCase()}`);
+  if (activeButton) {
+    activeButton.classList.add('active');
+  }
+});
+
+document.addEventListener('keyup', (event) => {
+  const activeButton = document.querySelector(`#${event.code.toLowerCase()}`);
+  if (activeButton) {
+    activeButton.classList.remove('active');
+  }
+});
